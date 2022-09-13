5594a5fed89ba1354b054d75f7444f326b6e44a1
rahimedu <edwin.duche82@gmail.com>
2022-09-12 23:08:22 -0400

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,10 @@ class ApplicationController < Sinatra::Base
     todo.to_json
   end
   
-  
+  delete "/todos/:id" do
+    todo = Todo.find(params[:id])
+    todo.destroy
+    todo.to_json
   end
   
 end
