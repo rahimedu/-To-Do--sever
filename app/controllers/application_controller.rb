73b7144c2421739cdc6d03bc15d63fcec465b8d2
rahimedu <edwin.duche82@gmail.com>
2022-09-12 22:44:20 -0400

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
-class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
-  
-  get "/todos" do
+
     todos = Todo.all.order(:created_at)
     todos.to_json
   end
