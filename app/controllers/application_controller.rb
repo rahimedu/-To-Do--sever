52fc1e357623ca20465f19f6060fac13e217bf7c
rahimedu <edwin.duche82@gmail.com>
2022-09-12 22:59:16 -0400

    refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  get "/todos" do
-    todos = Todo.all.order(:created_at)
-    todos.to_json
-  end
+ 
   
   post "/todos" do
     todo = Todo.create(tasks: params[:tasks] )
